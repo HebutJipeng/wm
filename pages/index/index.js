9d7aec8fff496a9d9c2a44bf74b2bb7a1691c8fe
HebutJipeng <282602893@qq.com>
2016-09-26 20:02:02 +0800

    转转转不了啊！

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,26 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    staffA: {firstName: 'Hulk', lastName: 'Hua'},
-    staffB: {firstName: 'Shang', lastName: 'You'},
-    staffC: {firstName: 'Giden', lastName: 'Lin'}
+    imgUrls: [
+      'http://cdn.q8oils.cc/3a6fbfed26f851fd32b739f384393bab@1e_420w_130h_1c_0i_1o_90Q_3-10bl_1x.png',
+      'http://cdn.q8oils.cc/119c52e9a4cd06cdfd2124d7debe7495@1e_420w_130h_1c_0i_1o_90Q_3-10bl_1x.png',
+      'http://cdn.q8oils.cc/215e1e0b5c7ef2cec3bd9a1c68a092b1@1e_420w_130h_1c_0i_1o_90Q_3-10bl_1x.png'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    btns: [
+      {
+        name1: 'btns1', content: ['btn1', 'btn2', 'btn3'] 
+      },
+      {
+        name1: 'btns2', content: ['btn4', 'btn5', 'btn6']
+      },
+      {
+        name1: 'btns3', content: ['btn7', 'btn8', 'btn9']
+      }
+    ]  
   },
   //事件处理函数
   bindViewTap: function() {
